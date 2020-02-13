325538f7b52c316f041e1f5e91b3dd08401b8a38
Jacob Allen <jacob@polyomino.xyz>
2020-02-13 01:11:38 +0000

    Updated README and restructured to a single package

--- src/pi_puck_base/src/pi_puck_base/utils.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Generic tools for the Pi-puck."""
-from socket import gethostname
-
-
-def get_pi_puck_name():
-    """Get the node name for the Pi-puck."""
-    return gethostname().replace("-", "_")
---
+++ src/pi_puck_driver/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(packages=['pi_puck_proximity', 'pi_puck_motors'], scripts=[], package_dir={'': 'src'})
+
+setup(**d)
