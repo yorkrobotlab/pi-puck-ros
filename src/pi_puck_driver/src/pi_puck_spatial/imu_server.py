e1aff5339d1a58e4cb14f6afadab4eb1ea75a663
Jacob Allen <jacob@polyomino.xyz>
2020-02-24 15:40:30 +0000

    Added linear acceleration and angular velocity to Imu server

--- src/pi_puck_driver/src/pi_puck_spatial/imu_server.py
+++ src/pi_puck_driver/src/pi_puck_spatial/imu_server.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 """ROS Node to expose a topics for the IMU (LSM9DS1) on the Pi-puck."""
 
+# Base Imports
+import math
+
 # ROS imports
 import rospy
-from sensor_msgs.msg import Temperature, Imu
+from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import Imu, Temperature
 
 # Standard imports
 from lsm9ds1 import LSM9DS1
 
 UNKNOWN_VARIANCE = 0
+
+# If accuracy is updated these need updating from the
+LINEAR_ACCELERATION_COVARIANCE = [0.061, 0, 0, 0, 0.061, 0, 0, 0, 0.061]
+ANGULAR_VELOCITY_COVARIANCE = [0.00875, 0, 0, 0, 0.00875, 0, 0, 0, 0.00875]
 
 
 class PiPuckImuServer:
@@ -35,23 +43,40 @@ class PiPuckImuServer:
         """Open the sensor."""
         self._sensor = LSM9DS1()
 
-    def read_sensor(self):
-        """Read the sensor."""
-        sensor_reading = self._sensor.get_distance()
-        if sensor_reading < MIN_RANGES[self._distance_mode]:
-            sensor_reading = -float("inf")
-        elif sensor_reading > MAX_RANGES[self._distance_mode]:
-            sensor_reading = float("inf")
-        sensor_reading = sensor_reading / 1000.0
-        return sensor_reading
-
     def run(self):
         """Run the sensor server."""
         self.open_sensor()
         while not rospy.is_shutdown():
+            # Temperature is in degrees C so no conversion needed
             temperature_result = self._sensor.temperature
             self._sensor_temperature_publisher.publish(
                 Temperature(temperature=temperature_result, variance=UNKNOWN_VARIANCE))
+
+            # Acceleration is already in m/s^2 so no conversion needed,
+            # just unpacking
+            acceleration_result = self._sensor.acceleration
+            acceleration_x, acceleration_y, acceleration_z = acceleration_result
+
+            # Gyro is in degrees/second so conversion to rads/sec is needed,
+            # as well as unpacking
+            gyro_result = self._sensor.gyro
+            gyro_x, gyro_y, gyro_z = map(lambda deg: deg * (math.pi / 180.0), gyro_result)
+
+            self._sensor_imu_publisher.publish(
+                Imu(linear_acceleration_covariance=LINEAR_ACCELERATION_COVARIANCE,
+                    linear_acceleration=Vector3(x=acceleration_x, y=acceleration_y, z=acceleration_z),
+                    angular_velocity_covariance=ANGULAR_VELOCITY_COVARIANCE,
+                    angular_velocity=Vector3(x=gyro_x, y=gyro_y, z=gyro_z)
+
+                    # Our IMU does not provide orientation information so we
+                    # can't send that as part of the IMU message
+
+                    # TODO: Implement orentation based on "compass"
+                    # We should be able to infer some from using the
+                    # magnetometer as a compass, though this might be low
+                    # accuracy and would mean making assumptions
+                    ))
+
             self._rate.sleep()
 
 
