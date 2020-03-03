cab05ef0841c058f3d45c1de046fb40ab2235755
Jacob Allen <jacob@polyomino.xyz>
2020-03-03 12:25:55 +0000

    Fixed radians vs degrees error

--- src/pi_puck_driver/src/pi_puck_spatial/imu_server.py
+++ src/pi_puck_driver/src/pi_puck_spatial/imu_server.py
@@ -23,7 +23,6 @@ ANGULAR_VELOCITY_COVARIANCE = [0.00875, 0, 0, 0, 0.00875, 0, 0, 0, 0.00875]
 # Rough estimate based on measurements with Pi-pucks
 CALIBRATION_DATA_DEFAULT = {"y": -3.10, "x": 0.15, "z": -1.70}
 
-def calculate_angle()
 
 class PiPuckImuServer:
     """ROS Node to publish data from the Pi-puck's IMU."""
@@ -57,12 +56,13 @@ class PiPuckImuServer:
 
     @staticmethod
     def euler_to_quaternion(yaw, pitch, roll):
-        double cy = math.cos(yaw * 0.5)
-        double sy = math.sin(yaw * 0.5)
-        double cp = math.cos(pitch * 0.5)
-        double sp = math.sin(pitch * 0.5)
-        double cr = math.cos(roll * 0.5)
-        double sr = math.sin(roll * 0.5)
+        """Convert euler angles of pitch, roll, and yaw to a quaternion."""
+        cy = math.cos(yaw * 0.5)
+        sy = math.sin(yaw * 0.5)
+        cp = math.cos(pitch * 0.5)
+        sp = math.sin(pitch * 0.5)
+        cr = math.cos(roll * 0.5)
+        sr = math.sin(roll * 0.5)
 
         q_w = cy * cp * cr + sy * sp * sr
         q_x = cy * cp * sr - sy * sp * cr
@@ -71,25 +71,29 @@ class PiPuckImuServer:
 
         return Quaternion(x=q_x, y=q_y, z=q_z, w=q_w)
 
-    @staticmethod
-    def calculate_heading_quaternion(magnetometer_reading):
+    def calculate_heading_quaternion(self, magnetometer_reading):
+        """Calculate the current orientation using magnetometer.
+
+        Currently only supports yaw calculation e.g. rotation about the z axis.
+        """
         raw_x, raw_y, raw_z = magnetometer_reading
         cal_x, cal_y, cal_z = self._calibration["x"], self._calibration["y"], self._calibration["z"]
+
         x, y, z = raw_x - cal_x, raw_y - cal_y, raw_z - cal_z
+
         if y == 0:
             if x > 0:
-                x_y_direction = 180
-            else: # x <= 0
-                x_y_direction = 0
+                x_y_direction = math.radians(180)
+            else:  # x <= 0
+                x_y_direction = math.radians(0)
         elif y > 0:
-            x_y_direction = 90 - math.atan(x/y)
-        else: # y < 0
-            x_y_direction = 270 - math.atan(x/y)
+            x_y_direction = math.radians(90) - math.atan(x / y)
+        else:  # y < 0
+            x_y_direction = math.radians(270) - math.atan(x / y)
 
-        x_y_direction_rad = x_y_direction * (math.pi / 180.0)
+        print(math.degrees(x_y_direction))
 
-        return euler_to_quaternion(yaw=x_y_direction_rad, 0, 0)
-
+        return PiPuckImuServer.euler_to_quaternion(yaw=x_y_direction, pitch=0, roll=0)
 
     def run(self):
         """Run the sensor server."""
@@ -111,15 +115,14 @@ class PiPuckImuServer:
             gyro_x, gyro_y, gyro_z = map(lambda deg: deg * (math.pi / 180.0), gyro_result)
 
             magnetometer_result = self._sensor.magnetic
-            magnetometer_quaternion = calculate_heading_quaternion(magnetometer_result)
+            magnetometer_quaternion = self.calculate_heading_quaternion(magnetometer_result)
 
             self._sensor_imu_publisher.publish(
                 Imu(linear_acceleration_covariance=LINEAR_ACCELERATION_COVARIANCE,
                     linear_acceleration=Vector3(x=acceleration_x, y=acceleration_y, z=acceleration_z),
                     angular_velocity_covariance=ANGULAR_VELOCITY_COVARIANCE,
                     angular_velocity=Vector3(x=gyro_x, y=gyro_y, z=gyro_z),
-                    orientation=magnetometer_quaternion
-                    ))
+                    orientation=magnetometer_quaternion))
 
             self._rate.sleep()
 
