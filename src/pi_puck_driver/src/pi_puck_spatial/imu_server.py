000f58e0c2e278aab85048ba5360d01049c361f9
Jacob Allen <jacob@polyomino.xyz>
2020-03-10 14:01:35 +0000

    Add magnetometer publisher

--- src/pi_puck_driver/src/pi_puck_spatial/imu_server.py
+++ src/pi_puck_driver/src/pi_puck_spatial/imu_server.py
@@ -10,7 +10,7 @@ from threading import Thread
 # ROS imports
 import rospy
 from geometry_msgs.msg import Vector3, Quaternion
-from sensor_msgs.msg import Imu, Temperature
+from sensor_msgs.msg import Imu, Temperature, MagneticField
 
 # Standard imports
 from lsm9ds1 import LSM9DS1
@@ -59,6 +59,10 @@ class PiPuckImuServer:
 
         rospy.init_node("imu")
 
+        self._calculate_full_orientation = bool(rospy.get_param("~calculate_full_orientation",
+                                                                True))
+        self._remove_gravity = bool(rospy.get_param("~remove_gravity", False))
+
         self._tf_prefix = rospy.get_param("tf_prefix", None)
 
         if self._tf_prefix is not None and not self._tf_prefix.endswith("/"):
@@ -70,7 +74,12 @@ class PiPuckImuServer:
         self._sample_rate = rospy.Rate(self._raw_sample_rate)
 
         self._sensor_imu_publisher = rospy.Publisher('imu/imu', Imu, queue_size=10)
-        self._sensor_temperature_publisher = rospy.Publisher('imu/temperature', Temperature, queue_size=10)
+        self._sensor_temperature_publisher = rospy.Publisher('imu/temperature',
+                                                             Temperature,
+                                                             queue_size=10)
+        self._sensor_magnetic_publisher = rospy.Publisher('imu/magnetic',
+                                                          MagneticField,
+                                                          queue_size=10)
 
         self._sensor = None
 
@@ -87,10 +96,13 @@ class PiPuckImuServer:
         else:
             self._calibration = CALIBRATION_DATA_DEFAULT
 
-        self._orientation_filter = MadgwickAHRS(sampleperiod=1.0 / float(self._raw_sample_rate), beta=AHRS_BETA)
+        if self._calculate_full_orientation:
+            self._orientation_filter = MadgwickAHRS(sampleperiod=1.0 / float(self._raw_sample_rate),
+                                                    beta=AHRS_BETA)
         self._acceleration_result = (0, 0, 0)
         self._orientation_quaternion = Quaternion(w=1, x=0, y=0, z=0)
         self._gyro_result = (0, 0, 0)
+        self._magnetometer_result = (0, 0, 0)
 
     def close_sensor(self):
         """Close the sensor after the ROS Node is shutdown."""
@@ -104,7 +116,8 @@ class PiPuckImuServer:
     def euler_to_quaternion(yaw, pitch, roll):
         """Convert euler angles of pitch, roll, and yaw to a quaternion.
 
-        Based on code from https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles.
+        Based on code from
+        https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles.
         """
         cy = math.cos(yaw * 0.5)
         sy = math.sin(yaw * 0.5)
@@ -126,8 +139,8 @@ class PiPuckImuServer:
         Currently only supports yaw calculation e.g. rotation about the z axis.
         """
         raw_x, raw_y, raw_z = magnetometer_reading
-        cal_x, cal_y, cal_z = self._calibration["magnetometer"]["x"], self._calibration["magnetometer"][
-            "y"], self._calibration["magnetometer"]["z"]
+        cal_x, cal_y, cal_z = self._calibration["magnetometer"]["x"], self._calibration[
+            "magnetometer"]["y"], self._calibration["magnetometer"]["z"]
 
         x, y, z = raw_x - cal_x, raw_y - cal_y, raw_z - cal_z
 
@@ -150,18 +163,20 @@ class PiPuckImuServer:
             # just unpacking
             acceleration_result = self._sensor.acceleration
             acceleration_x, acceleration_y, acceleration_z = acceleration_result
-            acceleration_x, acceleration_y, acceleration_z = acceleration_x - self._calibration["accelerometer"][
-                "x"], acceleration_y - self._calibration["accelerometer"]["y"], acceleration_z - self._calibration[
-                    "accelerometer"]["z"]
+            acceleration_x, acceleration_y, acceleration_z = acceleration_x - self._calibration[
+                "accelerometer"]["x"], acceleration_y - self._calibration["accelerometer"][
+                    "y"], acceleration_z - self._calibration["accelerometer"]["z"]
 
             # Gyro is in degrees/second so conversion to rads/sec is needed,
             # as well as unpacking
             gyro_result = map(lambda deg: deg * (math.pi / 180.0), self._sensor.gyro)
             gyro_x, gyro_y, gyro_z = gyro_result
-            gyro_x, gyro_y, gyro_z = gyro_x - self._calibration["gyro"]["x"], gyro_y - self._calibration["gyro"][
-                "y"], gyro_z - self._calibration["gyro"]["z"]
+            gyro_x, gyro_y, gyro_z = gyro_x - self._calibration["gyro"][
+                "x"], gyro_y - self._calibration["gyro"]["y"], gyro_z - self._calibration["gyro"][
+                    "z"]
 
-            # Magnetometer values can be used to calculate our orientation rotation about the z axis (yaw).
+            # Magnetometer values can be used to calculate our orientation rotation about the z
+            # axis (yaw).
             # Futher work is needed to calculate pitch and roll from other available sensors.
             magnetometer_result = self._sensor.magnetic
             # magnetometer_quaternion = self.calculate_heading_quaternion(magnetometer_result)
@@ -171,18 +186,34 @@ class PiPuckImuServer:
                                    magnetometer_result[1] - self._calibration["magnetometer"]["y"],
                                    magnetometer_result[2] - self._calibration["magnetometer"]["z"])
 
-            previous_orientation_quaternion = self._orientation_filter.quaternion
-            self._orientation_filter.update(gyro_result, acceleration_result, magnetometer_result)
+            if self._calculate_full_orientation:
+                previous_orientation_quaternion = self._orientation_filter.quaternion
+                self._orientation_filter.update(gyro_result, acceleration_result,
+                                                magnetometer_result)
 
-            if any(map(math.isnan, self._orientation_filter.quaternion)):
-                self._orientation_filter = MadgwickAHRS(sampleperiod=1.0 / float(self._raw_sample_rate),
-                                                        quaternion=previous_orientation_quaternion, beta=AHRS_BETA)
+                if any(map(math.isnan, self._orientation_filter.quaternion)):
+                    self._orientation_filter = MadgwickAHRS(
+                        sampleperiod=1.0 / float(self._raw_sample_rate),
+                        quaternion=previous_orientation_quaternion,
+                        beta=AHRS_BETA)
 
-            filter_w, filter_x, filter_y, filter_z = self._orientation_filter.quaternion
-            self._orientation_quaternion = Quaternion(w=filter_w, x=filter_x, y=filter_y, z=filter_z)
+                filter_w, filter_x, filter_y, filter_z = self._orientation_filter.quaternion
+                self._orientation_quaternion = Quaternion(w=filter_w,
+                                                          x=filter_x,
+                                                          y=filter_y,
+                                                          z=filter_z)
+            else:
+                self._orientation_quaternion = self.calculate_heading_quaternion(
+                    magnetometer_reading)
 
             self._gyro_result = (gyro_x, gyro_y, gyro_z)
+
+            if self._remove_gravity:
+                pass  # Not yet implemented
+
             self._acceleration_result = (acceleration_x, acceleration_y, acceleration_z)
+
+            self._magnetometer_result = magnetometer_result
 
             self._sample_rate.sleep()
 
@@ -194,24 +225,39 @@ class PiPuckImuServer:
         sensor_sample_thread.start()
 
         while not rospy.is_shutdown():
+            if self._tf_prefix:
+                tf_reference_frame = self._tf_prefix + REFERENCE_FRAME_ID
+            else:
+                tf_reference_frame = REFERENCE_FRAME_ID
+
             # Temperature is in degrees C so no conversion needed
-            temperature_message = Temperature(temperature=self._sensor.temperature, variance=UNKNOWN_VARIANCE)
-            temperature_message.header.frame_id = self._tf_prefix + REFERENCE_FRAME_ID if self._tf_prefix else REFERENCE_FRAME_ID
+            temperature_message = Temperature(temperature=self._sensor.temperature,
+                                              variance=UNKNOWN_VARIANCE)
+            temperature_message.header.frame_id = tf_reference_frame
             self._sensor_temperature_publisher.publish(temperature_message)
 
             acceleration_x, acceleration_y, acceleration_z = self._acceleration_result
             gyro_x, gyro_y, gyro_z = self._gyro_result
+            magnetometer_x, magnetometer_y, magnetometer_z = self._magnetometer_result
             orientation_quaternion = self._orientation_quaternion
 
             imu_message = Imu(linear_acceleration_covariance=LINEAR_ACCELERATION_COVARIANCE,
-                              linear_acceleration=Vector3(x=acceleration_x, y=acceleration_y, z=acceleration_z),
+                              linear_acceleration=Vector3(x=acceleration_x,
+                                                          y=acceleration_y,
+                                                          z=acceleration_z),
                               angular_velocity_covariance=ANGULAR_VELOCITY_COVARIANCE,
                               angular_velocity=Vector3(x=gyro_x, y=gyro_y, z=gyro_z),
                               orientation=orientation_quaternion,
                               orientation_covariance=ORIENTATION_COVARIANCE)
-            imu_message.header.frame_id = self._tf_prefix + REFERENCE_FRAME_ID if self._tf_prefix else REFERENCE_FRAME_ID
+            imu_message.header.frame_id = tf_reference_frame
 
             self._sensor_imu_publisher.publish(imu_message)
+
+            magnetic_message = MagneticField(
+                magnetic_field=Vector3(x=magnetometer_x, y=magnetometer_y, z=magnetometer_z))
+            magnetic_message.header.frame_id = tf_reference_frame
+
+            self._sensor_magnetic_publisher.publish(magnetic_message)
 
             self._rate.sleep()
 
