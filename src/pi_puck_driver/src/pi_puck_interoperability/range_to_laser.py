73044b0ea5c839e90322cdeca95dd179da2b1f1a
Jacob Allen <jacob@polyomino.xyz>
2020-03-27 16:43:56 +0000

    Make range to laser converter more configurable

--- src/pi_puck_driver/src/pi_puck_interoperability/range_to_laser.py
+++ src/pi_puck_driver/src/pi_puck_interoperability/range_to_laser.py
@@ -20,6 +20,8 @@ class RangeToLaserScan(object):
         rospy.init_node("range_to_laser")
 
         publish_combined_topic = bool(rospy.get_param('~publish_combined_topic', True))
+        publish_separate_topics = bool(rospy.get_param('~publish_separate_topics', False))
+        fov_adjustment = float(rospy.get_param('~fov_adjustment', 1.0))
         robot_root = rospy.get_param("~robot_root", rospy.get_namespace())
         range_publishers = tuple(topic
                                  for topic, topic_type in rospy.get_published_topics(robot_root)
@@ -33,17 +35,25 @@ class RangeToLaserScan(object):
             combined_topic = None
 
         for topic in range_publishers:
-            new_topic = posixpath.join(PREFIX, posixpath.relpath(topic, robot_root))
-            new_topic = posixpath.join(robot_root, new_topic)
-            publisher = rospy.Publisher(new_topic, LaserScan, queue_size=10)
+            if publish_separate_topics:
+                new_topic = posixpath.join(PREFIX, posixpath.relpath(topic, robot_root))
+                new_topic = posixpath.join(robot_root, new_topic)
+                publisher = rospy.Publisher(new_topic, LaserScan, queue_size=10)
+            else:
+                publisher = None
             publisher_handler = partial(self.republish_laser_scan,
-                                        publisher,
+                                        fov_adjustment=fov_adjustment,
+                                        publisher=publisher,
                                         combined_publisher=combined_topic)
             rospy.Subscriber(topic, Range, publisher_handler)
 
         self._running = True
 
-    def republish_laser_scan(self, publisher, data, combined_publisher=None):
+    def republish_laser_scan(self,
+                             data,
+                             fov_adjustment=1.0,
+                             publisher=None,
+                             combined_publisher=None):
         """Publish range as laser scan."""
         if not self._running:
             return
@@ -52,15 +62,17 @@ class RangeToLaserScan(object):
         laser_scan_message.range_max = data.max_range
         laser_scan_message.range_min = data.min_range
 
-        laser_scan_message.angle_min = -(data.field_of_view / 2)
-        laser_scan_message.angle_max = data.field_of_view / 2
-        laser_scan_message.angle_increment = data.field_of_view / 2
+        laser_scan_message.angle_min = -(data.field_of_view / (2 / fov_adjustment))
+        laser_scan_message.angle_max = data.field_of_view / (2 / fov_adjustment)
+        laser_scan_message.angle_increment = data.field_of_view / (2 / fov_adjustment)
 
         laser_scan_message.ranges = [data.range] * 3
 
         laser_scan_message.header.frame_id = data.header.frame_id
+        laser_scan_message.header.stamp = data.header.stamp
 
-        publisher.publish(laser_scan_message)
+        if publisher is not None:
+            publisher.publish(laser_scan_message)
 
         if combined_publisher is not None:
             combined_publisher.publish(laser_scan_message)
