fe1db5802615a3818586d396147ac4a30afa9560
Jacob Allen <jacob@polyomino.xyz>
2020-03-30 23:04:26 +0100

    Correct paste parameters

--- src/pi_puck_examples/src/pi_puck_examples/power_meter.py
+++ src/pi_puck_examples/src/pi_puck_examples/power_meter.py
@@ -25,7 +25,7 @@ def update_power_meter(data, publisher):
     draw = ImageDraw.Draw(image)
 
     draw.rectangle([(0, OLED_HEIGHT // 2), (int(
-        (OLED_WIDTH - 1) * battery_percent), OLED_HEIGHT - 1)],
+        OLED_WIDTH * battery_percent), OLED_HEIGHT)],
                    fill=0xff)
     draw.text((2, 2), str(int(battery_percent * 100)), font=FONT, fill=0xff)
 
